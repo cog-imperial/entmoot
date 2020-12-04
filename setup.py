d4d3ca19f0d3d2a7a275d0471fd0fd9f5915ac0e
ThebTron <alex.thebelt@googlemail.com>
2020-12-04 19:11:56 +0000

    fix installation and update version

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ setup(
         'numpy>=1.18.4',
         'scikit-learn>=0.21.3',
         'pyYAML>=5.3.1',
-        'lightgbm>=2.3.1',
+        'lightgbm>=2.3.1'
     ],
     setup_requires=[
         'numpy>=1.18.4',
         'scikit-learn>=0.21.3',
         'pyYAML>=5.3.1',
-        'lightgbm>=2.3.1',
+        'lightgbm>=2.3.1'
     ]
 )
 
