3319e56f60a811960da7b21373daca874c191059
ThebTron <alex.thebelt@googlemail.com>
2021-01-11 11:40:39 +0100

    replace check_random_state with sklearn import

--- entmoot/utils.py
+++ entmoot/utils.py
@@ -40,6 +40,7 @@ from joblib import dump as dump_
 from joblib import load as load_
 import numbers
 from .space import Space, Dimension
+from sklearn.utils import check_random_state
 
 def is_supported(base_estimator):
     from entmoot.learning.tree_model import EntingRegressor
@@ -401,25 +402,6 @@ def load(filename, **kwargs):
         Reconstructed OptimizeResult instance.
     """
     return load_(filename, **kwargs)
-
-def check_random_state(seed):
-    """Turn seed into a np.random.RandomState instance
-    Parameters
-    ----------
-    seed : None, int or instance of RandomState
-        If seed is None, return the RandomState singleton used by np.random.
-        If seed is an int, return a new RandomState instance seeded with seed.
-        If seed is already a RandomState instance, return it.
-        Otherwise raise ValueError.
-    """
-    if seed is None or seed is np.random:
-        return np.random.mtrand._rand
-    if isinstance(seed, numbers.Integral):
-        return np.random.RandomState(seed)
-    if isinstance(seed, np.random.RandomState):
-        return seed
-    raise ValueError('%r cannot be used to seed a numpy.random.RandomState'
-                     ' instance' % seed)
 
 def normalize_dimensions(dimensions):
     """Create a ``Space`` where all dimensions are normalized to unit range.
