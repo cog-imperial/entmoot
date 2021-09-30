9887718a088bcab3d24f917a4de4b43be1882da6
spiralulam <nathan.sudermann@gmail.com>
2021-09-30 07:19:57 +0200

    Cleaning up

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,6 @@
 import opti
 import pandas as pd
-from lightgbm import Booster
+import lightgbm as lgb
 
 from entmoot.optimizer import EntmootOpti
 
@@ -8,18 +8,17 @@ from entmoot.optimizer import EntmootOpti
 def test_api():
     # Definition of test problem
     test_problem = opti.problems.Zakharov_categorical(n_inputs=3)
-    test_problem.create_initial_data(15)
+    n_init = 15
+    test_problem.create_initial_data(n_init)
 
     # Declaration of entmoot instanceTrain surrogate model
     surrogat_params = {"verbose": -1, 'min_data_in_leaf': 5}
     entmoot = EntmootOpti(problem=test_problem, surrogat_params=surrogat_params)
 
-    assert entmoot.model is None
-
     # Train surrogate model
+    assert entmoot.model is None
     entmoot._fit_model()
-
-    assert type(entmoot.model) == Booster
+    assert type(entmoot.model) == lgb.Booster
 
     X_pred = pd.DataFrame([
         {"x0": 5, "x1": 5, "expon_switch": "one"},
@@ -28,14 +27,14 @@ def test_api():
 
     # Prediction based on surrogate model
     y_pred = entmoot.predict(X_pred)
-
     assert len(y_pred) == 2
 
     # Optimize acquisition function
-    n_proposals = 10
-    X_next = entmoot.propose(n_proposals=n_proposals)
-
-    assert len(X_next) == n_proposals
+    X_next = entmoot.propose(n_proposals=10)
+    assert len(X_next) == 10
 
     # Run Bayesian Optimization loop
-    entmoot.run()
+    n_steps = 3
+    n_proposals = 7
+    entmoot.run(n_steps=3, n_proposals=7)
+    assert len(entmoot.data) == n_init + n_steps*n_proposals
