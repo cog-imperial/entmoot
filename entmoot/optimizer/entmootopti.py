56db4d4ed730097495df7e381a12fe97b21019c9
spiralulam <nathan.sudermann@gmail.com>
2021-12-08 00:51:38 +0100

    Further work on propose method

--- entmoot/optimizer/entmootopti.py
+++ entmoot/optimizer/entmootopti.py
@@ -12,12 +12,10 @@ from entmoot.learning.gbm_model import GbmModel
 from entmoot.learning.lgbm_processing import order_tree_model_dict
 from entmoot.optimizer import Optimizer
 from entmoot.optimizer.gurobi_utils import (
-    add_gbm_to_gurobi_model,
     get_core_gurobi_model,
     get_gbm_obj,
 )
-from entmoot.space.space import Categorical, Integer, Real, Space
-from entmoot.utils import cook_estimator
+from entmoot.space.space import Categorical, Integer, Real
 
 
 class UncertaintyType(Enum):
@@ -59,6 +57,8 @@ class EntmootOpti(Algorithm):
             n_initial_points=0,
             num_obj=self.num_obj,
             random_state=73,
+            # core_model SOLL NICHT HIER UEBERGEBEN WERDEN, DA SONST DER GUROBI SERVER SCHON BEI DER INIT FUNKTION HOCHFAHREN MUSS.
+            # BITTE ERST SPAETER BEI DER PROPOSE METHODE
             #acq_optimizer_kwargs={
             #    "add_model_core": core_model
             #},
@@ -96,147 +96,64 @@ class EntmootOpti(Algorithm):
     def predict(self, X: pd.DataFrame) -> pd.DataFrame:
         return self.entmoot_optimizer.predict_with_acq(X.to_numpy().tolist())
 
-    def _get_gbm_model(self):
-        original_tree_model_dict = self.model.dump_model()
-        import json
-        with open('tree_dict_next.json', 'w') as f:
-            json.dump(
-                original_tree_model_dict,
-                f,
-                indent=4,
-                sort_keys=False
-            )
-
-        ordered_tree_model_dict = \
-            order_tree_model_dict(
-                original_tree_model_dict,
-                cat_column=self.cat_idx
-            )
-
-        gbm_model = GbmModel(ordered_tree_model_dict)
-        return gbm_model
-
     def propose(self, n_proposals: int = 1, uncertainty_type: UncertaintyType = UncertaintyType.DDP) -> pd.DataFrame:
-        X_res = pd.DataFrame(columns=self.problem.inputs.names)
-        X_std_data = self._encode_cat_vars(self.problem.data[self.problem.inputs.names]).values
-        y_std_data = self.problem.get_Y()
 
-        if self.gurobi_env is not None:
-            env = self.gurobi_env()
-        else:
-            env = None
+        core_model = get_core_gurobi_model()
 
-        for _ in range(n_proposals):
-
-            # build gurobi core model
-            gurobi_model: gurobipy.Model = get_core_gurobi_model(self._space, env=env)
-
-            # Shut down logging
-            gurobi_model.Params.OutputFlag = 0
-            gurobi_model.Params.TimeLimit = 60
-
-            # add lgbm core structure to gurobi model
-            gbm_model_dict = {}
-            gbm_model_dict['first'] = self._get_gbm_model()
-            add_gbm_to_gurobi_model(
-                self._space, gbm_model_dict, gurobi_model
-            )
-            gurobi_model.update()
-
-            # Migrate constraints from opti to gurobi
-            if self.problem.constraints:
-                for c in self.problem.constraints:
-                    if isinstance(c, opti.constraint.LinearInequality):
-                        coef = {x: a for (x, a) in zip(c.names, c.lhs)}
-                        gurobi_model.addConstr(
-                            (
-                                sum(
-                                    coef[v.varname] * v
-                                    for v in gurobi_model.getVars()
-                                    if v.varname in coef
-                                )
-                                <= c.rhs
-                            ),
-                            name="LinearInequalityOpti"
-                        )
-                    elif isinstance(c, opti.constraint.LinearEquality):
-                        coef = {x: a for (x, a) in zip(c.names, c.lhs)}
-                        gurobi_model.addConstr(
-                            (
-                                sum(
-                                    coef[v.varname] * v
-                                    for v in gurobi_model.getVars()
-                                    if v.varname in coef
-                                )
-                                == c.rhs
-                            ),
-                            name="LinearEqualityOpti"
-                        )
-                    elif isinstance(c, opti.constraint.NChooseK):
-                        # Big-M implementation of n-choose-k constraint
-                        y = gurobi_model.addVars(c.names, vtype=gurobipy.GRB.BINARY)
-                        gurobi_model.addConstrs(
-                            (
-                                y[v.varname] * v.lb <= v
+        # Migrate constraints from opti to gurobi
+        if self.problem.constraints:
+            for c in self.problem.constraints:
+                if isinstance(c, opti.constraint.LinearInequality):
+                    coef = {x: a for (x, a) in zip(c.names, c.lhs)}
+                    gurobi_model.addConstr(
+                        (
+                            sum(
+                                coef[v.varname] * v
                                 for v in gurobi_model.getVars()
-                                if v.varname in c.names
-                            ),
-                            name="n-choose-k-constraint LB",
-                        )
-                        gurobi_model.addConstrs(
-                            (
-                                y[v.varname] * v.ub >= v
+                                if v.varname in coef
+                            )
+                            <= c.rhs
+                        ),
+                        name="LinearInequalityOpti"
+                    )
+                elif isinstance(c, opti.constraint.LinearEquality):
+                    coef = {x: a for (x, a) in zip(c.names, c.lhs)}
+                    gurobi_model.addConstr(
+                        (
+                            sum(
+                                coef[v.varname] * v
                                 for v in gurobi_model.getVars()
-                                if v.varname in c.names
-                            ),
-                            name="n-choose-k-constraint UB",
-                        )
-                        gurobi_model.addConstr(
-                            y.sum() == c.max_active, name="max active components"
-                        )
-                    else:
-                        raise ValueError(f"Constraint of type {type(c)} not supported.")
+                                if v.varname in coef
+                            )
+                            == c.rhs
+                        ),
+                        name="LinearEqualityOpti"
+                    )
+                elif isinstance(c, opti.constraint.NChooseK):
+                    # Big-M implementation of n-choose-k constraint
+                    y = gurobi_model.addVars(c.names, vtype=gurobipy.GRB.BINARY)
+                    gurobi_model.addConstrs(
+                        (
+                            y[v.varname] * v.lb <= v
+                            for v in gurobi_model.getVars()
+                            if v.varname in c.names
+                        ),
+                        name="n-choose-k-constraint LB",
+                    )
+                    gurobi_model.addConstrs(
+                        (
+                            y[v.varname] * v.ub >= v
+                            for v in gurobi_model.getVars()
+                            if v.varname in c.names
+                        ),
+                        name="n-choose-k-constraint UB",
+                    )
+                    gurobi_model.addConstr(
+                        y.sum() == c.max_active, name="max active components"
+                    )
+                else:
+                    raise ValueError(f"Constraint of type {type(c)} not supported.")
 
-            # add std to gurobi model
-            std_estimator_params = {}
-            std_est = cook_std_estimator(uncertainty_type.name, self._space, std_estimator_params=std_estimator_params)
-            std_est.cat_idx = self.cat_idx
-
-            std_est.update(X_std_data, y_std_data, cat_column=self.cat_idx)
-            std_est.add_to_gurobi_model(gurobi_model)
-            gurobi_model.update()
-
-            # add obj to gurobi model
-            mu = get_gbm_obj(gurobi_model)
-            std = std_est.get_gurobi_obj(gurobi_model, scaled=False)
-            kappa = 1.96
-            ob_expr = gurobipy.quicksum((mu, kappa * std))
-            gurobi_model.setObjective(ob_expr, gurobipy.GRB.MINIMIZE)
-            gurobi_model.update()
-
-            # Optimize gurobi model
-            gurobi_model.optimize()
-
-            # Get optimization results
-            x_next = np.empty(len(self._space.dimensions))
-            # cont features
-            for i in gurobi_model._cont_var_dict:
-                x_next[i] = gurobi_model._cont_var_dict[i].x
-            # cat features
-            for i in gurobi_model._cat_var_dict:
-                cat = [k for k in gurobi_model._cat_var_dict[i] if round(gurobi_model._cat_var_dict[i][k].x, 1) == 1]
-                x_next[i] = cat[0]
-
-            # Constant liar strategy where new y-value is chosen as minimal observation
-            X_std_data = np.vstack([X_std_data, x_next])
-            y_std_data = np.vstack([y_std_data, sum(y_std_data)/len(y_std_data)])
-
-            X_next_df_enc = pd.DataFrame(x_next.reshape(1, -1), columns=self.problem.inputs.names)
-
-            X_next_df = X_next_df_enc.copy()
-            for cat in self.cat_decode_mapping:
-                X_next_df[cat] = X_next_df[cat].replace(self.cat_decode_mapping[cat])
-
-            X_res = X_res.append(X_next_df)
+            X = self.entmoot_optimizer.ask(n_points=n_proposals)
 
         return X_res
