47fe7f4e11470883a537be5c9a139040044e9971
at1618 <alexander.thebelt18@imperial.ac.uk>
2022-11-10 11:47:21 +0000

    add base classes for categorical uncertainty metrics

--- entmoot/models/enting.py
+++ entmoot/models/enting.py
@@ -4,6 +4,9 @@ from entmoot.models.uncertainty_models.euclidean_squared_distance import Euclide
 from entmoot.models.uncertainty_models.l1_distance import L1Distance
 from entmoot.models.uncertainty_models.l2_distance import L2Distance
 
+from entmoot.models.uncertainty_models.overlap_distance import OverlapDistance
+from entmoot.models.uncertainty_models.goodall4_distance import Goodall4Distance
+from entmoot.models.uncertainty_models.of_distance import OfDistance
 
 class Enting(BaseModel):
     def __init__(self, problem_config, params=None):
@@ -24,7 +27,9 @@ class Enting(BaseModel):
         dist_metric = unc_params.get("dist_metric", "euclidean_squared")
         dist_trafo = unc_params.get("dist_trafo", "normalize")
         acq_sense = unc_params.get("acq_sense", "exploration")
+        cat_metric = unc_params.get("cat_metric", "overlap")
         self._beta = unc_params.get("beta", 1.96)
+        self._non_cat_x, self._cat_x = None, None
 
         assert dist_trafo in ('normal', 'standard'), \
             f"Pick 'dist_trafo' '{dist_trafo}' in '('normalize', 'standardize')'."
@@ -34,17 +39,33 @@ class Enting(BaseModel):
 
         # pick distance metric for non-cat features
         if dist_metric == "euclidean_squared":
-            self.unc_model = EuclideanSquaredDistance(
-                problem_config=self._problem_config, dist_trafo=dist_trafo, acq_sense=acq_sense)
+            self.non_cat_unc_model = EuclideanSquaredDistance(
+                problem_config=self._problem_config, acq_sense=acq_sense, dist_trafo=dist_trafo)
         elif dist_metric == "l1":
-            self.unc_model = L1Distance(
-                problem_config=self._problem_config, dist_trafo=dist_trafo, acq_sense=acq_sense)
+            self.non_cat_unc_model = L1Distance(
+                problem_config=self._problem_config, acq_sense=acq_sense, dist_trafo=dist_trafo)
         elif dist_metric == "l2":
-            self.unc_model = L2Distance(
-                problem_config=self._problem_config, dist_trafo=dist_trafo, acq_sense=acq_sense)
+            self.non_cat_unc_model = L2Distance(
+                problem_config=self._problem_config, acq_sense=acq_sense, dist_trafo=dist_trafo)
         else:
-            raise IOError(f"Uncertainty type '{dist_metric}' for {self.__class__.__name__} model "
-                          f"is not supported. Check 'params['uncertainty_type']'.")
+            raise IOError(f"Non-categorical uncertainty metric '{dist_metric}' for "
+                          f"{self.__class__.__name__} model is not supported. "
+                          f"Check 'params['uncertainty_type']'.")
+
+        # pick distance metric for cat features
+        if cat_metric == "overlap":
+            self.cat_unc_model = OverlapDistance(
+                problem_config=self._problem_config, acq_sense=acq_sense)
+        elif cat_metric == "of":
+            self.cat_unc_model = L1Distance(
+                problem_config=self._problem_config, acq_sense=acq_sense)
+        elif cat_metric == "goodall4":
+            self.cat_unc_model = L2Distance(
+                problem_config=self._problem_config, acq_sense=acq_sense)
+        else:
+            raise IOError(
+                f"Categorical uncertainty metric '{cat_metric}' for {self.__class__.__name__} "
+                f"model is not supported. Check 'params['uncertainty_type']'.")
 
     @property
     def non_cat_x(self):
---
+++ entmoot/models/uncertainty_models/overlap_distance.py
@@ -0,0 +1,15 @@
+from entmoot.models.uncertainty_models.base_distance import BaseDistance
+
+class OverlapDistance(BaseDistance):
+
+    def _get_distance(self, x_left, x_right):
+        raise NotImplementedError()
+
+    def _array_predict(self, X):
+        raise NotImplementedError()
+
+    def _add_to_gurobipy_model(self, model_core):
+        raise NotImplementedError()
+
+    def _add_pyomo_model(self, model_core):
+        raise NotImplementedError()
