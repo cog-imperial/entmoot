d9a0181995e7c3b952d25777c9062868a4c59629
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-10 22:40:41 +0100

    Try to fix an rtd error

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,33 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../entmoot'))
-sys.path.insert(1, os.path.abspath('..'))
-sys.path.insert(2, os.path.abspath('../..'))
+import inspect
+
+__location__ = os.path.join(
+    os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe()))
+)
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.join(__location__, "../src"))
+
+# -- Run sphinx-apidoc -------------------------------------------------------
+# This hack is necessary since RTD does not issue `sphinx-apidoc` before running
+# `sphinx-build -b html . _build/html`. See Issue:
+# https://github.com/rtfd/readthedocs.org/issues/1139
+# DON'T FORGET: Check the box "Install your project inside a virtualenv using
+# setup.py install" in the RTD Advanced Settings.
+# Additionally it helps us to avoid running apidoc manually
+
+try:  # for Sphinx >= 1.7
+    from sphinx.ext import apidoc
+except ImportError:
+    from sphinx import apidoc
+
+#sys.path.insert(0, os.path.abspath('../entmoot'))
+#sys.path.insert(1, os.path.abspath('..'))
+#sys.path.insert(2, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
