97615b5e761dc7314240006e65bb46f6bbf9580b
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-10 21:04:59 +0100

    Try to fix module not found error

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../entmoot/'))
+sys.path.insert(0, os.path.abspath('../entmoot'), os.path.abspath('..'), os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
