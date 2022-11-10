572ee97a311dc0fe9722a791bb8640c557a86d33
at1618 <alexander.thebelt18@imperial.ac.uk>
2022-11-10 10:40:12 +0000

    add base distance to uncertainty models

---
+++ entmoot/models/uncertainty_models/base_distance.py
@@ -0,0 +1,60 @@
+from entmoot.models.base_model import BaseModel
+import numpy as np
+
+
+class BaseDistance(BaseModel):
+
+    def __init__(self, problem_config, params=None):
+        if params is None:
+            params = {}
+
+        self._problem_config = problem_config
+        self._acq_sense = params.get("acq_sense", "exploration")
+        self._dist_trafo = params.get("dist_trafo", "standard")
+
+        self._shift, self._scale = None, None
+
+        assert self._dist_trafo in ('normal', 'standard'), \
+            "Parameter 'dist_trafo' for uncertainty model needs to be " \
+            "in '('normal', 'standard')'."
+
+        assert self._acq_sense in ('exploration', 'penalty'), \
+            "Parameter 'acq_sense' for uncertainty model needs to be " \
+            "in '('exploration', 'penalty')'."
+
+    @property
+    def shift(self):
+        return self._shift
+
+    @property
+    def scale(self):
+        return self._scale
+
+    def _get_distance(self, x_left, x_right):
+        raise NotImplementedError()
+
+    def predict(self, X):
+        raise NotImplementedError()
+
+    def _array_predict(self, X):
+        raise NotImplementedError()
+
+    def fit(self, X, y):
+        # define shift and scalar values for non-cat feats
+        if self._dist_trafo == "normal":
+            self._shift = np.asarray(self._problem_config.non_cat_lb)
+            self._scale = np.asarray(self._problem_config.non_cat_bnd_diff)
+        elif self._dist_trafo == "standard":
+            self._shift = np.mean(np.asarray(X[:, self._problem_config.non_cat_idx]), axis=0)
+            self._scale = np.std(np.asarray(X[:, self._problem_config.non_cat_idx]), axis=0)
+        else:
+            raise IOError("Parameter 'dist_trafo' for uncertainty model needs to be "
+                          "in '('normal', 'standard')'.")
+        self._X = X
+        self._y = y
+
+    def _add_to_gurobipy_model(self, model_core):
+        raise NotImplementedError()
+
+    def _add_pyomo_model(self, model_core):
+        raise NotImplementedError()
