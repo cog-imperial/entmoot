8bfbc5873c73e8811304b113e6566e1cecbefcc0
at1618 <alexander.thebelt18@imperial.ac.uk>
2022-11-15 17:26:42 +0000

    add basic obj implementations

--- entmoot/models/enting.py
+++ entmoot/models/enting.py
@@ -19,6 +19,17 @@ class Enting(BaseModel):
 
         # initialize unc model
         unc_params = params.get("unc_params", {})
+        self._acq_sense = unc_params.get("acq_sense", "exploration")
+        assert self._acq_sense in ('exploration', 'penalty'), \
+            f"Pick 'acq_sense' '{self._acq_sense}' in '('exploration', 'penalty')'."
+
+        self._beta = unc_params.get("beta", 1.96)
+        assert self._beta >= 0.0, \
+            f"Value for 'beta' is {self._beta} but must be '>= 0.0'."
+
+        if self._acq_sense == "exploration":
+            self._beta = -self._beta
+
         self.unc_model = DistanceBasedUncertainty(problem_config=problem_config, params=unc_params)
 
     def fit(self, X, y):
@@ -63,11 +74,31 @@ class Enting(BaseModel):
 
         return comb_pred
 
+    def predict_pareto(self):
+        pass
+
+    def predict_acq(self, X):
+        acq_pred = []
+        comb_pred = self.predict(X)
+        for mean, unc in comb_pred:
+            acq_pred.append(mean + self._beta * unc)
+        return acq_pred
+
+
     def add_to_gurobipy_model(self, core_model):
-        self.mean_model.add_to_gurobipy_model(core_model)
+        from gurobipy import GRB
+
+        self.mean_model.add_to_gurobipy_model(core_model, add_mu_var=True)
         self.unc_model.add_to_gurobipy_model(core_model)
 
     def _add_to_pyomo_model(self, core_model):
+        # single objective case
+        if len(self._problem_config.obj_list) == 1:
+            core_model.setObjective(core_model._mu[0] + self._beta * core_model._unc)
+
+        # TODO: obj trafo build into model mu and returned by tree model
+
+    def _add_pyomo_model(self, core_model):
         raise NotImplementedError()
 
     def update_params(params):
---
+++ entmoot/utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+from scipy.special import comb
+
+
+def grid(dimension: int, levels: int) -> np.ndarray:
+    """Construct a regular grid on the unit simplex.
+
+    The number of grid points is L = (n+m-1)!/(n!*(m-1)!)
+    where m is the dimension and n+1 is the number of levels.
+
+    Args:
+        dimension (int): Number of variables.
+        levels (int): Number of levels for each variable.
+
+    Returns:
+       array: Regularily spaced points on the unit simplex.
+
+    Examples:
+        --> simplex_grid(3, 3)
+        array([
+            [0. , 0. , 1. ],
+            [0. , 0.5, 0.5],
+            [0. , 1. , 0. ],
+            [0.5, 0. , 0.5],
+            [0.5, 0.5, 0. ],
+            [1. , 0. , 0. ]
+        ])
+
+    References:
+        Nijenhuis and Wilf, Combinatorial Algorithms, Chapter 5, Academic Press, 1978.
+    """
+    m = dimension
+    n = levels - 1
+    L = int(comb(dimension - 1 + levels - 1, dimension - 1, exact=True))
+
+    x = np.zeros(m, dtype=int)
+    x[-1] = n
+
+    out = np.empty((L, m), dtype=int)
+    out[0] = x
+
+    h = m
+    for i in range(1, L):
+        h -= 1
+
+        val = x[h]
+        x[h] = 0
+        x[h - 1] += 1
+        x[-1] = val - 1
+
+        if val != 1:
+            h = m
+
+        out[i] = x
+
+    return out / n
+
+
+def sample(dimension: int, n_samples: int = 1) -> np.ndarray:
+    """Sample uniformly from the unit simplex.
+
+    Args:
+        dimension (int): Number of dimensions.
+        n_samples (int): Number of samples to draw.
+
+    Returns:
+        array, shape=(n_samples, dimesnion): Random samples from the unit simplex.
+    """
+    s = np.random.standard_exponential((n_samples, dimension))
+    return (s.T / s.sum(axis=1)).T
