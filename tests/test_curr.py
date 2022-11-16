dd9b0c37986140a6efb4482261139e33b23994dc
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-16 01:35:30 +0100

    added some dummy tests

--- tests/test_curr.py
+++ tests/test_curr.py
@@ -5,7 +5,7 @@ import pyomo.environ as pyo
 
 
 @pytest.mark.fast_test
-def test_tree_model_definition():
+def test_tree_model_definition_multiobj_l2():
     def test_func(X):
         X = np.atleast_2d(X)
         return np.sin(X[:, 0])
@@ -29,7 +29,7 @@ def test_tree_model_definition():
     problem_config.add_min_objective()
 
     # sample data
-    rnd_sample = problem_config.get_rnd_sample_list(num_samples=20)
+    rnd_sample = problem_config.get_rnd_sample_list(num_samples=20, seed=73)
     pred = test_func_multi_obj(rnd_sample)
 
     # fit tree ensemble
@@ -53,9 +53,23 @@ def test_tree_model_definition():
     # Enrich Gurobi and Pyomo models by constraints from tree model and objective function. The objective function
     # contains an acquisition and an uncertainty part.
     enting.add_to_gurobipy_model(model_core_gurobi)
-    enting.add_to_pyomo_model(model_core_pyomo)
+    # enting.add_to_pyomo_model(model_core_pyomo)
 
     # Assert that both models contain the same number of variables:
     # assert len(model_core_gurobi.getVars()) == sum(len(x) for x in model_core_pyomo.component_objects(pyo.Var))
     # Assert that both models contain the same number of constraints:
     # assert len(model_core_gurobi.getConstrs()) == sum(len(x) for x in model_core_pyomo.component_objects(pyo.Constraint))
+
+
+@pytest.mark.fast_test
+def test_tree_model_definition_singleobj_l2():
+    pass
+
+
+@pytest.mark.fast_test
+def test_tree_model_definition_multiobj_l1():
+    pass
+
+@pytest.mark.fast_test
+def test_tree_model_definition_singleobj_l1():
+    pass
