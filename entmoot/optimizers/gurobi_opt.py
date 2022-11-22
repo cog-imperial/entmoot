39bc81bb4698b9ce4c2f2e59ec898628db8ebdb1
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-22 23:22:31 +0100

    added type hints and comments

--- entmoot/optimizers/gurobi_opt.py
+++ entmoot/optimizers/gurobi_opt.py
@@ -1,9 +1,13 @@
+from entmoot.problem_config import ProblemConfig
+from entmoot.models.enting import Enting
+import gurobipy as gur
+
 class GurobiOptimizer:
-    def __init__(self, problem_config, params=None):
+    def __init__(self, problem_config: ProblemConfig, params: dict = None) -> float:
         self._params = {} if params is None else params
         self._problem_config = problem_config
 
-    def solve(self, model, model_core=None, weights=None):
+    def solve(self, tree_model: Enting, model_core: gur.Model = None, weights: tuple = None):
         if model_core is None:
             opt_model = self._problem_config.get_gurobi_model_core()
         else:
@@ -15,18 +19,22 @@ class GurobiOptimizer:
             assert len(weights) == len(self._problem_config.obj_list), \
                 f"Number of 'weights' is '{len(weights)}', number of objectives " \
                 f"is '{len(self._problem_config.obj_list)}'."
-            assert sum(weights) == 1.0, f"'weights' don't add up to '1.0'"
+            assert sum(weights) == 1.0, "weights don't add up to 1.0"
 
         # set solver parameters
         for param, param_val in self._params.items():
             opt_model.setParam(param, param_val)
 
         # build gurobi model
-        model.add_to_gurobipy_model(opt_model, weights=weights)
+        tree_model.add_to_gurobipy_model(opt_model, weights=weights)
+
+        # Solve optimization model
         opt_model.optimize()
+
+        return opt_model.ObjVal
 
     def get_next_x(self):
         raise NotImplementedError()
 
-    def sample_feas(self, model_core, num_points):
+    def sample_feas(self, model_core: gur.Model, num_points):
         raise NotImplementedError()
