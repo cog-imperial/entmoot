7884f9defa8e2f842ba41af7edcf1e38cda26b40
at1618 <alexander.thebelt18@imperial.ac.uk>
2022-11-23 14:15:08 +0000

    remove unneeded functions

--- entmoot/models/uncertainty_models/overlap_distance.py
+++ entmoot/models/uncertainty_models/overlap_distance.py
@@ -4,9 +4,3 @@ from entmoot.models.uncertainty_models.base_distance import CatDistance
 class OverlapDistance(CatDistance):
     def _sim_mat_rule(self, x_left, x_right, cat_idx):
         return 1 if x_left == x_right else 0
-
-    def _array_predict(self, X):
-        raise NotImplementedError()
-
-    def add_to_pyomo_model(self, model_core):
-        raise NotImplementedError()
