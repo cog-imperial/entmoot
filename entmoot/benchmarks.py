49496909d5df1bb063861fa37ef118c70ca4b0cf
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-23 12:05:34 +0100

    single objective version added

--- entmoot/benchmarks.py
+++ entmoot/benchmarks.py
@@ -3,17 +3,17 @@ from numpy.typing import ArrayLike
 from typing import Iterable
 
 
-def multi_obj_categorical(X: ArrayLike) -> np.ndarray:
+def multi_obj_categorical(X: ArrayLike, n_obj: int = 2) -> np.ndarray:
     """
-    Benchmark function with at least four input variables and two outputs.
-    param X: Usually a numpy array or a list of tuples. Each row (or tuple) consists of at least four entries with the
+    Benchmark function with at least four input variables and one or two outputs
+    :param X: Usually a numpy array or a list of tuples. Each row (or tuple) consists of at least four entries with the
     following properties:
     - The first one is a categorical variable with the possible values "blue", "orange" and "gray".
     - The second one is an integer variable.
     - The third one is a binary variable.
     - The fourth one and all additional variables are real numbers.
-    :return: A numpy array with two columns that contains the objectives values for each data point
-    :rtype: Numpy array
+    :param n_obj: number of objectives (one or two)
+    :return: objective value(s) for each data point
     """
 
     # without the dtype=object paramer, each entry of X is converted into a string
@@ -32,11 +32,16 @@ def multi_obj_categorical(X: ArrayLike) -> np.ndarray:
             )
         elif xi[0] == "gray":
             return(
-            xi[2]*xi[3]**xi[1],
-            -(1-xi[2]) * xi[3] ** xi[1]
+                xi[2]*xi[3]**xi[1],
+                -(1-xi[2]) * xi[3] ** xi[1]
             )
         else:
             raise IOError(f"You provided the illegal value {xi[0]} for the categorical variable. Allowed values are "
                           f"'blue', 'orange' and 'gray'")
-
-    return np.array([compute_objectives(xi) for xi in X])
+    if n_obj == 2:
+        return np.array([compute_objectives(xi) for xi in X])
+    elif n_obj == 1:
+        return np.array([[sum(compute_objectives(xi))/2] for xi in X])
+    else:
+        raise IOError(f"You provided the illegal value {n_obj} for the number of objectives. "
+                      f"Allowed values are 1 and 2")
