ba4bf5f080d9caede688aed4dfb2660883a4a765
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-23 12:06:02 +0100

    removed redundancies and added single objective tests

--- tests/test_curr.py
+++ tests/test_curr.py
@@ -1,7 +1,5 @@
 from entmoot.problem_config import ProblemConfig
-import numpy as np
 import pytest
-import pyomo.environ as pyo
 import os
 from entmoot.benchmarks import multi_obj_categorical
 
@@ -10,7 +8,7 @@ from entmoot.benchmarks import multi_obj_categorical
 @pytest.mark.skipif(
     "CICD_ACTIVE" in os.environ, reason="No optimization runs in CICD pipelines"
 )
-def test_tree_model_definition_multiobj_l2():
+def test_compare_pyomo_gurobipy_multiobj():
 
     # define problem
     problem_config = ProblemConfig(rnd_seed=73)
@@ -31,36 +29,31 @@ def test_tree_model_definition_multiobj_l2():
 
     # fit tree ensemble
     from entmoot.models.enting import Enting
+    for metric in ["l1", "l2"]:
+        params = {"unc_params": {"dist_metric": metric}}
+        enting = Enting(problem_config, params=params)
+        enting.fit(rnd_sample, pred)
 
-    params = {"unc_params": {"dist_metric": "l2"}}
-    enting = Enting(problem_config, params=params)
-    enting.fit(rnd_sample, pred)
+        # Build GurobiOptimizer object and solve optimization problem
+        from entmoot.optimizers.gurobi_opt import GurobiOptimizer
+        params_gurobi = {"NonConvex": 2, "MIPGap": 0}
+        opt_gur = GurobiOptimizer(problem_config, params=params_gurobi)
+        optval_gur = opt_gur.solve(enting, weights=(0.5, 0.5))
 
-    # Build GurobiOptimizer object and solve optimization problem
-    from entmoot.optimizers.gurobi_opt import GurobiOptimizer
-    params_gurobi = {"NonConvex": 2, "MIPGap": 0}
-    opt_gur = GurobiOptimizer(problem_config, params=params_gurobi)
-    optval_gur = opt_gur.solve(enting, weights=(0.5, 0.5))
+        # Build PyomoOptimizer object with Gurobi as solver and solve optimization problem
+        from entmoot.optimizers.pyomo_opt import PyomoOptimizer
+        params_pyo = {"solver_name": "gurobi", "solver_options": {"NonConvex": 2, "MIPGap": 0}}
+        pyo_opt = PyomoOptimizer(problem_config, params=params_pyo)
+        optval_pyo = pyo_opt.solve(enting, weights=(0.5, 0.5))
 
-    # Build PyomoOptimizer object with Gurobi as solver and solve optimization problem
-    from entmoot.optimizers.pyomo_opt import PyomoOptimizer
-    params_pyo = {"solver_name": "gurobi", "solver_options": {"NonConvex": 2, "MIPGap": 0}}
-    pyo_opt = PyomoOptimizer(problem_config, params=params_pyo)
-    optval_pyo = pyo_opt.solve(enting, weights=(0.5, 0.5))
-
-    assert round(optval_gur, 3) == round(optval_pyo, 3)
-
-
-@pytest.mark.fast_test
-def test_tree_model_definition_singleobj_l2():
-    pass
+        assert round(optval_gur, 2) == round(optval_pyo, 2)
 
 
 @pytest.mark.fast_test
 @pytest.mark.skipif(
     "CICD_ACTIVE" in os.environ, reason="No optimization runs in CICD pipelines"
 )
-def test_tree_model_definition_multiobj_l1():
+def test_compare_pyomo_gurobipy_singleobj():
 
     # define problem
     problem_config = ProblemConfig(rnd_seed=73)
@@ -73,34 +66,28 @@ def test_tree_model_definition_multiobj_l1():
     problem_config.add_feature("real", (5.0, 6.0))
 
     problem_config.add_min_objective()
-    problem_config.add_min_objective()
 
     # sample data
     rnd_sample = problem_config.get_rnd_sample_list(num_samples=20)
-    pred = multi_obj_categorical(rnd_sample)
+    pred = multi_obj_categorical(rnd_sample, n_obj=1)
 
     # fit tree ensemble
     from entmoot.models.enting import Enting
+    for metric in ["l1", "l2"]:
+        params = {"unc_params": {"dist_metric": metric}}
+        enting = Enting(problem_config, params=params)
+        enting.fit(rnd_sample, pred)
 
-    params = {"unc_params": {"dist_metric": "l1"}}
-    enting = Enting(problem_config, params=params)
-    enting.fit(rnd_sample, pred)
+        # Build GurobiOptimizer object and solve optimization problem
+        from entmoot.optimizers.gurobi_opt import GurobiOptimizer
+        params_gurobi = {"NonConvex": 2, "MIPGap": 0}
+        opt_gur = GurobiOptimizer(problem_config, params=params_gurobi)
+        optval_gur = opt_gur.solve(enting)
 
-    # Build GurobiOptimizer object and solve optimization problem
-    from entmoot.optimizers.gurobi_opt import GurobiOptimizer
-    params_gurobi = {"NonConvex": 2, "MIPGap": 0}
-    opt_gur = GurobiOptimizer(problem_config, params=params_gurobi)
-    optval_gur = opt_gur.solve(enting, weights=(0.5, 0.5))
+        # Build PyomoOptimizer object with Gurobi as solver and solve optimization problem
+        from entmoot.optimizers.pyomo_opt import PyomoOptimizer
+        params_pyo = {"solver_name": "gurobi", "solver_options": {"NonConvex": 2, "MIPGap": 0}}
+        pyo_opt = PyomoOptimizer(problem_config, params=params_pyo)
+        optval_pyo = pyo_opt.solve(enting)
 
-    # Build PyomoOptimizer object with Gurobi as solver and solve optimization problem
-    from entmoot.optimizers.pyomo_opt import PyomoOptimizer
-    params_pyo = {"solver_name": "gurobi", "solver_options": {"NonConvex": 2, "MIPGap": 0}}
-    pyo_opt = PyomoOptimizer(problem_config, params=params_pyo)
-    optval_pyo = pyo_opt.solve(enting, weights=(0.5, 0.5))
-
-    assert round(optval_gur, 3) == round(optval_pyo, 3)
-
-
-@pytest.mark.fast_test
-def test_tree_model_definition_singleobj_l1():
-    pass
+        assert round(optval_gur, 2) == round(optval_pyo, 2)
