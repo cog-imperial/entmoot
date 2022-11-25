c3630740dacb983dfb9d0f12cdc1cf41ce989c51
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-25 21:23:06 +0100

    Some restructuring

--- tests/test_curr.py
+++ tests/test_curr.py
@@ -1,18 +1,16 @@
 from entmoot.problem_config import ProblemConfig
 import pytest
 import os
-from entmoot.benchmarks import multi_obj_categorical
+from entmoot.benchmarks import eval_multi_obj_cat_testfunc
 
 
-@pytest.mark.fast_test
-@pytest.mark.skipif(
-    "CICD_ACTIVE" in os.environ, reason="No optimization runs in CICD pipelines"
-)
-def test_compare_pyomo_gurobipy_multiobj():
-
-    # define problem
-    problem_config = ProblemConfig(rnd_seed=73)
-
+def build_multi_obj_categorical_problem(problem_config: ProblemConfig, n_obj: int =2):
+    """
+    Builds a small test example which is frequently used by the tests.
+    :param problem_config: ProblemConfig object where features and objectives are added
+    :param n_obj: Number of objectives
+    :return: None, the problem definition happens "inplace"
+    """
     problem_config.add_feature("categorical", ("blue", "orange", "gray"))
     problem_config.add_feature("integer", (5, 6))
     problem_config.add_feature("binary")
@@ -20,18 +18,33 @@ def test_compare_pyomo_gurobipy_multiobj():
     problem_config.add_feature("real", (4.6, 6.0))
     problem_config.add_feature("real", (5.0, 6.0))
 
-    problem_config.add_min_objective()
-    problem_config.add_min_objective()
+    for _ in range(n_obj):
+        problem_config.add_min_objective()
+
+
+@pytest.mark.fast_test
+@pytest.mark.skipif(
+    "CICD_ACTIVE" in os.environ, reason="No optimization runs in CICD pipelines"
+)
+def test_compare_pyomo_gurobipy_multiobj():
+    """
+    Ensures for a multi objective example with l1  and l2 uncertainty metric and mixed feature types that optimization
+    results for Gurobipy model and Pyomo model with Gurobi as optimizer coincide.
+    """
+
+    # define problem
+    problem_config = ProblemConfig(rnd_seed=73)
+    build_multi_obj_categorical_problem(problem_config, n_obj=2)
 
     # sample data
     rnd_sample = problem_config.get_rnd_sample_list(num_samples=20)
-    pred = multi_obj_categorical(rnd_sample)
+    pred = eval_multi_obj_cat_testfunc(rnd_sample)
 
-    # fit tree ensemble
     from entmoot.models.enting import Enting
     for metric in ["l1", "l2"]:
         params = {"unc_params": {"dist_metric": metric}}
         enting = Enting(problem_config, params=params)
+        # fit tree ensemble
         enting.fit(rnd_sample, pred)
 
         # Build GurobiOptimizer object and solve optimization problem
@@ -54,28 +67,24 @@ def test_compare_pyomo_gurobipy_multiobj():
     "CICD_ACTIVE" in os.environ, reason="No optimization runs in CICD pipelines"
 )
 def test_compare_pyomo_gurobipy_singleobj():
+    """
+    Ensures for a single objective example with l1  and l2 uncertainty metric and mixed feature types that optimization
+    results for Gurobipy model and Pyomo model with Gurobi as optimizer coincide.
+    """
 
     # define problem
     problem_config = ProblemConfig(rnd_seed=73)
-
-    problem_config.add_feature("categorical", ("blue", "orange", "gray"))
-    problem_config.add_feature("integer", (5, 6))
-    problem_config.add_feature("binary")
-    problem_config.add_feature("real", (5.0, 6.0))
-    problem_config.add_feature("real", (4.6, 6.0))
-    problem_config.add_feature("real", (5.0, 6.0))
-
-    problem_config.add_min_objective()
+    build_multi_obj_categorical_problem(problem_config, n_obj=1)
 
     # sample data
     rnd_sample = problem_config.get_rnd_sample_list(num_samples=20)
-    pred = multi_obj_categorical(rnd_sample, n_obj=1)
+    pred = eval_multi_obj_cat_testfunc(rnd_sample, n_obj=1)
 
-    # fit tree ensemble
     from entmoot.models.enting import Enting
     for metric in ["l1", "l2"]:
         params = {"unc_params": {"dist_metric": metric}}
         enting = Enting(problem_config, params=params)
+        # fit tree ensemble
         enting.fit(rnd_sample, pred)
 
         # Build GurobiOptimizer object and solve optimization problem
@@ -91,3 +100,13 @@ def test_compare_pyomo_gurobipy_singleobj():
         optval_pyo = pyo_opt.solve(enting)
 
         assert round(optval_gur, 2) == round(optval_pyo, 2)
+
+    def test_tree_model_vs_opt_model():
+        """
+        This test compares the prediction values from the tree models with the corresponding decision variable
+        model._unscaled_mu in order to check, if the tree model was incorporated correctly in the optimization model.
+        """
+        pass
+
+        # TODO: Kleines Beispiel, wo Optimum bekannt ist: Wird es errechnet?
+        # TODO: Test, if Pyomo user can run ENTMOOT without having gurobipy installed and vice versa
