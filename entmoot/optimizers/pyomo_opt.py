7718aaf5a5dc748e168ae6ee70a146c49d0ef103
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-25 20:45:41 +0100

    added methods and parameter to store and retrieve current solutions

--- entmoot/optimizers/pyomo_opt.py
+++ entmoot/optimizers/pyomo_opt.py
@@ -1,16 +1,22 @@
 from entmoot.problem_config import ProblemConfig
 from entmoot.models.enting import Enting
+import pyomo.environ as pyo
 
 
 class PyomoOptimizer:
     def __init__(self, problem_config: ProblemConfig, params: dict = None):
         self._params = {} if params is None else params
         self._problem_config = problem_config
+        self._curr_sol = None
 
-    def solve(self, tree_model: Enting, model_core=None, weights=None):
+    @property
+    def get_curr_sol(self):
+        assert (
+            self._curr_sol is not None
+        ), "No solution was generated yet."
+        return self._curr_sol
 
-        import pyomo.environ as pyo
-
+    def solve(self, tree_model: Enting, model_core: pyo.ConcreteModel = None, weights=None):
         if model_core is None:
             opt_model = self._problem_config.get_pyomo_model_core()
         else:
@@ -36,9 +42,26 @@ class PyomoOptimizer:
         tree_model.add_to_pyomo_model(opt_model)
 
         # Solve optimization model
-        opt.solve(opt_model)
+        opt.solve(opt_model, tee=True)
+
+        # update current solution
+        self._curr_sol = self._get_sol(opt_model)
 
         return pyo.value(opt_model.obj)
+
+    def _get_sol(self, solved_model):
+        res = []
+        for idx, feat in enumerate(self._problem_config.feat_list):
+            curr_var = solved_model._all_feat[idx]
+            if feat.is_cat():
+                # find active category
+                sol_cat = [int(round(pyo.value(curr_var[enc_cat]))) for enc_cat in feat.enc_cat_list].index(1)
+                res.append(sol_cat)
+            else:
+                res.append(pyo.value(curr_var))
+        print(res)
+
+        return self._problem_config.decode([res])
 
     def sample_feas(num_points):
         raise NotImplementedError()
