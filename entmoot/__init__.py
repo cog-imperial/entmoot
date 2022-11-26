43fbbb389bcaafb933807a91c67e54398f0e1720
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2022-11-26 22:30:43 +0100

    enables flat import

---
+++ entmoot/__init__.py
@@ -0,0 +1,4 @@
+from entmoot.problem_config import ProblemConfig
+from entmoot.models.enting import Enting
+from entmoot.optimizers.gurobi_opt import GurobiOptimizer
+from entmoot.optimizers.pyomo_opt import PyomoOptimizer
