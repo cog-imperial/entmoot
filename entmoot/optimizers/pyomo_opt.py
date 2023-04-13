54ccac2b2579186735c0dad3eba2e5e1144e5ed5
Nathan Sudermann-Merx <45530936+spiralulam@users.noreply.github.com>
2023-04-13 10:55:24 +0200

    integrated Alex' changes on the Pyomo side regarding active leaves

--- entmoot/optimizers/pyomo_opt.py
+++ entmoot/optimizers/pyomo_opt.py
@@ -9,6 +9,7 @@ class PyomoOptimizer:
         self._params = {} if params is None else params
         self._problem_config = problem_config
         self._curr_sol = None
+        self._active_leaves = None
 
     @property
     def get_curr_sol(self) -> list:
@@ -18,8 +19,15 @@ class PyomoOptimizer:
         assert self._curr_sol is not None, "No solution was generated yet."
         return self._curr_sol
 
+    def get_active_leaf_sol(self) -> list:
+        """
+        returns current solution (i.e. optimal points) from optimization based
+        """
+        assert self._curr_sol is not None, "No solution was generated yet."
+        return self._active_leaves
+
     def solve(
-        self, tree_model: Enting, model_core: pyo.ConcreteModel = None, weights=None
+        self, tree_model: Enting, model_core: pyo.ConcreteModel = None, weights: tuple = None
     ) -> namedtuple:
         """
         Solves the Pyomo optimization model
@@ -53,7 +61,7 @@ class PyomoOptimizer:
         opt.solve(opt_model, tee=True)
 
         # update current solution
-        self._curr_sol = self._get_sol(opt_model)
+        self._curr_sol, self._active_leaves = self._get_sol(opt_model)
 
         return OptResult(
             self.get_curr_sol,
@@ -62,6 +70,7 @@ class PyomoOptimizer:
         )
 
     def _get_sol(self, solved_model: pyo.ConcreteModel) -> list:
+        # extract solutions from conti and discrete variables
         res = []
         for idx, feat in enumerate(self._problem_config.feat_list):
             curr_var = solved_model._all_feat[idx]
@@ -74,6 +83,18 @@ class PyomoOptimizer:
                 res.append(sol_cat)
             else:
                 res.append(pyo.value(curr_var))
-        print(res)
 
-        return self._problem_config.decode([res])
+        # extract active leaves of solution
+        def obj_leaf_index(model_obj, obj_name):
+            # this function is the same as in 'tree_ensemble.py', TODO: put this in a tree_utils?
+            for tree in range(model_obj._num_trees(obj_name)):
+                for leaf in model_obj._leaves(obj_name, tree):
+                    yield tree, leaf
+
+        act_leaves = []
+        for idx, obj in enumerate(self._problem_config.obj_list):
+            act_leaves.append(
+                [(tree_id, leaf_enc) for tree_id, leaf_enc in obj_leaf_index(solved_model, obj.name)
+                 if round(pyo.value(solved_model._z[obj.name, tree_id, leaf_enc])) == 1.0])
+
+        return self._problem_config.decode([res]), act_leaves
