a62cea8444ddb43484227a25fcb7fd8810baf71c
Toby Boyne <tobyboyne@gmail.com>
2024-12-31 18:13:36 +0000

    Update version

--- setup.py
+++ setup.py
@@ -17,6 +17,16 @@ setup(
     version=about["__version__"],
     url="https://github.com/cog-imperial/entmoot",
     packages=find_packages(exclude=["tests", "docs"]),
-    install_requires=["numpy<=2.0.0", "lightgbm>=4.0.0", "gurobipy", "pyomo==6.7.1"],
-    setup_requires=["numpy<=2.0.0", "lightgbm>=4.0.0", "gurobipy", "pyomo==6.7.1"],
+    install_requires=[
+        "numpy<=2.0.0",
+        "lightgbm>=4.0.0",
+        "gurobipy<12.0.0",
+        "pyomo==6.7.1",
+    ],
+    setup_requires=[
+        "numpy<=2.0.0",
+        "lightgbm>=4.0.0",
+        "gurobipy<12.0.0",
+        "pyomo==6.7.1",
+    ],
 )
